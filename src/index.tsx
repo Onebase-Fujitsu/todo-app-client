0ec650740a1e1014111a2168284123e2ec96c720
onebase <toast20191106@gmail.com>
2021-09-09 22:47:25 +0900

    add tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
+import './index.css'
 import App from './App'
 import {store} from './stores/store'
 
