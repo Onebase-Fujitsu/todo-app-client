4da7e80b3225509b4c910bf04e6001b171f662ae
onebase <toast20191106@gmail.com>
2021-09-09 23:35:59 +0900

    add enzyme

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
     ],
     'react/react-in-jsx-scope': 'off',
     'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
   settings: {
     'import/resolver': {
