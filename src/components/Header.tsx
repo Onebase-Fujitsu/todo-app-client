76ead9ac43c3edcd30379fa87787adb0161a0db3
onebase <toast20191106@gmail.com>
2021-09-10 03:48:08 +0900

    apply header design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => (
   <div className="flex items-center bg-green-500 p-6">
-    <h1 className="font-semibold text-xl text-white tracking-tight">Journal App</h1>
+    <h1 className="font-semibold text-xl text-white tracking-tight">Todo App</h1>
   </div>
 )
 
