5ab39e9555cd4761ab6ad0a721104e3cc1633913
onebase <toast20191106@gmail.com>
2021-09-14 17:05:44 +0900

    apply design to TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,23 @@
 import {useSelector} from 'react-redux'
+import {ArchiveIcon} from "@heroicons/react/solid";
 import {RootState} from '../stores/store'
 
 const TodoList = () => {
   const todos = useSelector((state: RootState) => state.todos)
 
   return (
-    <ul data-testid="TodoList">
+    <ul data-testid="TodoList" className="w-full p-8 flex flex-col">
       {todos.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
+        <li key={todo.id} className="w-full flex justify-center items-center pb-4">
+          <div className="border-2 flex justify-between border-gray-200 w-full rounded-lg shadow-lg p-4">
+            <div>
+              <p className="text-xl font-bold">{todo.title}</p>
+            </div>
+            <button type="button">
+              <ArchiveIcon className="w-6"/>
+            </button>
+          </div>
+        </li>
       ))}
     </ul>
   )
